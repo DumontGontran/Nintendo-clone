519fd4621f891da1e75da16446b26bf6df0dfc74
DumontGontran <dumont.gontran@orange.fr>
2023-12-16 21:19:04 +0100

    homepage added

---
+++ src/pages/Home.page.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header.component'
+import SideMenu from '../components/SideMenu.component';
+
+const Home = () => {
+    return (
+        <main>
+            <Header />
+            <SideMenu />
+        </main>
+    );
+};
+
+export default Home;
