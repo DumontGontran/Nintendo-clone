d80e68aaf905f8b4ac7704b61bc6c5bf50d4250e
DumontGontran <dumont.gontran@orange.fr>
2023-12-16 21:18:43 +0100

    error404 page added

---
+++ src/components/Error.component.tsx
@@ -0,0 +1,10 @@
+const Error = () => {
+    return (
+        <main className='flex flex-col bg-red-600 justify-center items-center h-screen'>
+        <h1 className='text-white text-8xl font-bold pb-10'>ERROR 404.</h1>
+        <h2 className='text-white text-4xl font-bold'>Cette page n'existe pas !</h2>
+        </main>
+    )
+}
+
+export default Error
